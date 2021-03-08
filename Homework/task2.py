82d698d2a8a5513cc1d0a90f6276629ad5f0a8bf
IldarYakup <IldarYakup@gmail.com>
2021-03-08 18:09:09 +0300

    3 задачи из третьего урока

---
+++ Homework/task2.py
@@ -0,0 +1,22 @@
+"""
+Во втором массиве сохранить индексы четных элементов первого массива.
+Например, если дан массив со значениями 8, 3, 15, 6, 4, 2,
+второй массив надо заполнить значениями 0, 3, 4, 5, (индексация начинается с нуля),
+т.к. именно в этих позициях первого массива стоят четные числа.
+ """
+import random
+
+SIZE = 10
+MIN_ITEM = 0
+MAX_ITEM = 100
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print(array)
+second_array = []
+third_array = []
+
+for i in array:
+    if i%2==0:
+        second_array.append(array.index(i))
+        third_array.append(i)
+print(f'Четные числа указаны в списке {third_array}')
+print(f'И имеют индексы {second_array}')
---
+++ Homework/task3.py
@@ -0,0 +1,30 @@
+"""
+В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
+"""
+import random
+
+SIZE = 10
+MIN_ITEM = 0
+MAX_ITEM = 100
+array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
+print(array)
+max_num = array[0]
+for i in range(1, len(array)):
+    if array[i] > max_num:
+        max_num = array[i]
+min_num = array[0]
+for i in range(1, len(array)):
+    if array[i] < min_num:
+        min_num = array[i]
+print(f'Максимальное число в списке - {max_num} и находится на {array.index(max_num)+1} позиции списка')
+print(f'Минимальное число в списке - {min_num} и находится на {array.index(min_num)+1} позиции списка')
+
+first_el = array[array.index(max_num)]
+second_el = array[array.index(min_num)]
+first_index = array.index(max_num)
+second_index = array.index(min_num)
+array.insert(first_index, second_el)
+array.pop(first_index+1)
+array.insert(second_index, first_el)
+array.pop(second_index+1)
+print(f'Производим замену местами максимального и минимального значения\n{array}')
