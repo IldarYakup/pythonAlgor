6cb061c24cedf984ff9a58e59fb51cf3c9526537
IldarYakup <IldarYakup@gmail.com>
2021-03-15 16:05:17 +0300

    Начало положено

--- homework/task1.py
+++ homework/task1.py
@@ -6,3 +6,19 @@
 
 from collections import defaultdict
 
+user_company = int(input("Введите количество предприятий: "))
+dict = []
+for i in range(user_company):
+    name = input("Введите название компании: ")
+    quarter_i = float(input("Введите прибыль за I квартал: "))
+    quarter_ii = float(input("Введите прибыль за II квартал: "))
+    quarter_iii = float(input("Введите прибыль за III квартал: "))
+    quarter_iv = float(input("Введите прибыль за IV квартал: "))
+    company = {"name": name,
+               "I квартал": quarter_i,
+               "II квартал": quarter_ii,
+               "III квартал": quarter_iii,
+               "IV квартал": quarter_iv,
+               "Средняя прибыль": (quarter_i + quarter_ii + quarter_iii + quarter_iv)/4}
+    dict.append(company)
+print(dict)
